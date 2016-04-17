ba2e0b6e73c6e8788ba171530b74ae7595c24cf1
magicwifi <huangzhe1218@163.com>
2016-04-17 20:23:19 +0800

    finish rest

--- app/controllers/bus_numbers_controller.rb
+++ app/controllers/bus_numbers_controller.rb
@@ -20,10 +20,23 @@ def index
  @bus_numbers = BusNumber.all
 end
 
+def edit
+ @bus_number = BusNumber.find(params[:id])
+end
+
+def update
+@bus_number = BusNumber.find(params[:id])
+if @bus_number.update_attributes(bus_number_params)
+redirect_to bus_numbers_url
+else
+render 'edit' 
+end
+end
+
 def destroy
         BusNumber.find(params[:id]).destroy
         flash[:success] = "删除成功"
-        redirect_to sites_url
+        redirect_to bus_numbers_url
 end
 
 def new
@@ -33,6 +46,8 @@ end
 def create
 @bus_number = BusNumber.new(bus_number_params) 
 if @bus_number.save
+bus_driver = BusDriver.where(:bus_number_id=>nil).last
+bus_driver.update_columns(:bus_number_id=>@bus_number.id)
 flash[:success] = "添加成功!"
 redirect_to bus_numbers_url
 else
--- app/controllers/bus_routes_controller.rb
+++ app/controllers/bus_routes_controller.rb
@@ -17,8 +17,25 @@ end
 def destroy
         BusRoute.find(params[:id]).destroy
         flash[:success] = "删除成功"
-        redirect_to sites_url
+        redirect_to bus_routes_url
 end
 
+def edit
+ @bus_route = BusRoute.find(params[:id])
+end
+
+def update
+@bus_route = BusRoute.find(params[:id])
+if @bus_route.update_attributes(bus_route_params)
+redirect_to bus_routes_url
+else
+render 'edit' 
+end
+end
+
+private 
+def bus_route_params
+  params.require(:bus_route).permit(:name)
+end
 
 end
