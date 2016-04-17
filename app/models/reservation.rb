d248a54f3a347beabb2a76361ccb2d3babb6cdb3
magicwifi <huangzhe1218@163.com>
2016-04-17 15:43:58 +0800

    today

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,9 @@ class Reservation < ActiveRecord::Base
 belongs_to :bus_number
 belongs_to :passenger
 belongs_to :site
+validates :bus_number_id, presence: true 
+validates :passenger_id, presence: true 
+validates :site_id, presence: true
 
 def can_cancel?
  bn = self.bus_number
